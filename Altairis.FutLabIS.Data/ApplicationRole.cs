f19e887291b7f9b297f29fb1d20844b60b7191b1
Jirka Novak <jirka@nones.cz>
2021-01-02 05:13:10 +0100

    Add ASP.NET Identity

---
+++ Altairis.FutLabIS.Data/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Altairis.FutLabIS.Data
+{
+    public class ApplicationRole : IdentityRole<int>
+    {
+        public const string Member = "Member";
+        public const string Master = "Master";
+        public const string Administrator = "Administrator";
+    }
+}
---
+++ Altairis.FutLabIS.Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Altairis.FutLabIS.Data
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public bool Enabled { get; set; } = true;
+        [Required, MinLength(2), MaxLength(2)]
+        public string Language { get; set; }
+    }
+}
